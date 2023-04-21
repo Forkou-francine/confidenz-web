4a467466a63b917b704aa1174d330c5670871d4f
Forkou-francine <ange.pene@institutsaintjean.org>
2023-04-21 20:10:01 +0100

    Ajout du projet sur git, premier commit

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('Requete recue');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({message: 'Votre requête a bien été reçue !'});
+});
+
+app.use((req, res) => {
+    console.log('Réponse envoyée avec succès !');
+    next();
+});
+
+module.exports = app;
