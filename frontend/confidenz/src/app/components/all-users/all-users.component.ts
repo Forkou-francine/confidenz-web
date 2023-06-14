4d0e83b7b41b874a3e64d69c031ad67a95faffad
Forkou-francine <ange.pene@institutsaintjean.org>
2023-06-14 12:56:54 +0100

    Modification des tabeaux pour ajouter les filtres et la pagination

--- frontend/confidenz/src/app/components/all-users/all-users.component.ts
+++ frontend/confidenz/src/app/components/all-users/all-users.component.ts
@@ -5,11 +5,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from '../profile/profile.component';
 import { DetailsComponent } from '../details/details.component';
 
+import { AlertService } from 'src/app/services/alert.service';
 import { LoginService } from 'src/app/services/login.service';
 import { HttpService } from 'src/app/services/http.service';
 import { Users } from 'src/app/classes/users';
 import { HomeComponent } from '../home/home.component';
 import { RegisterComponent } from '../register/register.component';
+import { AlertComponent } from '../alert/alert.component';
 
 const routes: Routes = [
   {
@@ -33,7 +35,8 @@ export class AllUsersComponent implements OnInit{
   searchText: any;
 
   constructor( private logServive: LoginService,
-              private httpService: HttpService){}
+              private httpService: HttpService,
+              private alertService: AlertService){}
 
 
   ngOnInit(){
@@ -48,10 +51,23 @@ export class AllUsersComponent implements OnInit{
   deleteUser(id: string){
     const user = this.users!.filter(x => x.id != id);
     //user.isDeleting = true;
+    this.alertService.clear();
     this.httpService.delete(id)
     .pipe(first())
-    .subscribe(() => this.users = this.users!.filter(x=> x.id !=id))
+    .subscribe(() => this.users = this.users!.filter(x=> x.id !=id));
+    this.alertService.success('User deleted successfully', { keepAfterRouteChange: true });
   }
+
+  saveUser(use: Users){
+    //const user = this.users!.filter(x => x.id != id);
+    this.alertService.clear();
+    //user.isDeleting = true;
+    // this.httpService.register(use)
+    // .pipe(first())
+   // .subscribe(() => this.users = this.users!.filter(x=> x.id !=id
+   this.alertService.success('User deleted successfully', { keepAfterRouteChange: true })
+  }
+
 
 
 
